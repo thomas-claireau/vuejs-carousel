fc7cbb5c08d9449fbbe4890e00439897847e33ac
Thomas Claireau <thomas.claireau@gmail.com>
2020-06-14 19:00:08 +0200

    add informations to deploy cpanel

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x
+
+# Build script
+cd ~/app.thomas-claireau.fr/vuejs-carousel/
+source ~/.bashrc
+npm install
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production' ? '/vuejs-carousel/dist/' : '/',
 	chainWebpack: (config) => {
 		config.module.rules.delete('svg');
 	},
